7768b3526006f08215b90e234724b0a254263bdd
XCaminhante <caminhante@i2pmail.org>
2019-10-29 17:49:08 -0300

    More CCAs

--- listmap.h
+++ listmap.h
@@ -7,9 +7,9 @@ interface $ListMap {
   object ListMap  (funcptr new) (string key, pointer value, objptr(ListMap) next);
   // [ valid ListMap object -> deallocates its `->key` and `->value` entries | <Identity> ]
   void            (funcptr destroy) (me(ListMap));
-  // [ valid ListMap chain -> Number of elements in the chain | 0 ]
+  // [ valid ListMap chain -> number of elements in the chain | 0 ]
   size_t          (funcptr size) (me(ListMap));
-  // [ valid ListMap chain -> Nth ListMap entry at the chain | 0 ]
+  // [ valid ListMap chain -> nth object at the chain | 0 ]
   objptr(ListMap) (funcptr nth) (me(ListMap), size_t index);
   // [ valid ListMap chain and an ListMap entry has this key
   // (a key different from a empty string) ->
--- stack.h
+++ stack.h
@@ -5,8 +5,11 @@
 
 interface $Stack {
   object Stack  (funcptr new)     (pointer data, objptr(Stack) next);
+  // [ valid Stack object -> deallocates its `->value` entry | <Identity> ]
   void          (funcptr destroy) (me(Stack));
+  // [ valid Stack chain -> number of elements in the chain | 0 ]
   size_t        (funcptr size)    (me(Stack));
+  // [ valid Stack chain -> nth object at the chain | 0 ]
   objptr(Stack) (funcptr nth)     (me(Stack), size_t index);
 };
 
