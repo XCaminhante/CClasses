35348309d81969187f5226848918276a365a7d94
XCaminhante <caminhante@i2pmail.org>
2019-12-19 19:50:12 -0300

    Oops

--- class.h
+++ class.h
@@ -6,3 +6,24 @@ typedef object $Message msg_t;
 typedef object $Object obj_t;
 typedef object $Type type_t;
 
+typedef const unsigned long $subject;
+typedef bool (funcptr $method) (me($Object), objptr($Message) msg);
+
+interface $Type {
+  $method _m;
+};
+
+interface $Object {
+  object $Type _type;
+};
+
+interface $Message {
+  objptr($Object) _obj;
+  $subject _msg;
+};
+
+#define subject(m) (($subject)(objptr($Message))(m)->_msg)
+#define dispatcher(obj) (($method)((objptr($Object))(obj))->_type._m)
+#define send(obj,msg) dispatcher(obj)(obj,msg)
+#define send2(obj,msg) ({ mutable($Object,*msg._obj)=obj;dispatcher(obj)(obj,msg); })
+
