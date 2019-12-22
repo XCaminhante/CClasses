41b6222b26b36202a104a9ebe6b3d909aa1fa0ad
XCaminhante <caminhante@i2pmail.org>
2019-12-22 04:25:18 +0000

    Minor consistency modifications

--- t.c
+++ t.c
@@ -1,18 +1,18 @@
 #include "class.h"
 
-interface Stack$Type {
+interface {
   $method _handler;
   $method new;
   $method destroy;
   $method size;
   $method nth;
-} Stack$Type = {0};
+} Stack$Type;
 
 bool Stack$Handler (me($Class), objptr($Message) msg) {
-  if (is_subject(msg,Stack$Type.new))           puts("New");
-  else if (is_subject(msg,Stack$Type.destroy))  puts("Destroy");
-  else if (is_subject(msg,Stack$Type.size))     puts("Size");
-  else if (is_subject(msg,Stack$Type.nth))      puts("Nth");
+  if (is_subject(msg,Stack$Type.new)) {}
+  else if (is_subject(msg,Stack$Type.destroy))
+  else if (is_subject(msg,Stack$Type.size))
+  else if (is_subject(msg,Stack$Type.nth))
   else puts("Stack$Handler: Undefined method error");
   mutable($Class,msg->_return) = NULL;
   return true;
@@ -25,8 +25,8 @@ class Stack$Class {
 } Stack$Class = { {Stack$Handler}, NULL, NULL };
 
 int main () {
-  object Stack$Class st = Stack$Class;
-  object $Message m = { ._subject = &Stack$Type.new };
+  object Stack$Class st = new(Stack);
+  object $Message m = new_message(Stack,new);
   send(&st,&m);
   return 0;
 }
