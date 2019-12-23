3e2cd3c6b0d4eb469a97849e4a822af2a106849b
XCaminhante <caminhante@i2pmail.org>
2019-12-23 21:19:02 +0000

    Classes now receive a `$Class const*const*` reference

--- class.h
+++ class.h
@@ -6,7 +6,8 @@ typedef object $Message msg_t;
 typedef object $Class class_t;
 typedef object $Type type_t;
 
-typedef bool (funcptr $method) (me($Class), objptr($Message) msg);
+typedef bool (funcptr $method) \
+  (objptr($Class) iterptr me, objptr($Message) msg);
 
 interface $Type {
   $method _handler;
@@ -21,8 +22,14 @@ interface $Message {
   $method funcptr _subject;
 };
 
-#define is_subject(MSG,METHOD) ((MSG)->_subject == &(METHOD))
-#define handler(OBJ) ((OBJ)->_type._handler)
-#define send(OBJ,MSG) handler(OBJ)((objptr($Class))OBJ,(objptr($Message))MSG)
+#define is_subject(MSG,METHOD) \
+  ((MSG)->_subject == &(METHOD))
+#define def_handler(NAME) \
+  bool NAME (objptr($Class) iterptr me, objptr($Message) msg)
+#define handler(OBJ) \
+  ((* OBJ)->_type._handler)
+#define send(OBJ,MSG) \
+  handler(OBJ)((objptr($Class) iterptr)OBJ,(objptr($Message))MSG)
 
-#define new_message(METHOD,...) { ._subject=&(METHOD), __VA_ARGS__ }
+#define new_message(METHOD,...) \
+  { (object $Message) {._subject=&(METHOD)}, __VA_ARGS__ }
