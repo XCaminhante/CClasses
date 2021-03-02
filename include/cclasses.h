60e4b54c61d8d2c570c4d4344124726f4da653dd
XCaminhante <caminhante@i2pmail.org>
2021-03-02 09:34:59 -0300

    Standardization and int to const void * bug

--- include/cclasses.h
+++ include/cclasses.h
@@ -119,7 +119,7 @@ interface $Message {
 #define cast_msg(TYPE,METHOD) \
   ((objptr(message(TYPE,METHOD)))msg)
 #define new(CLASS,INITVAL) \
-  { {handler(CLASS)}, ({INITVAL||0;}) }
+  { {handler(CLASS)}, ({INITVAL||default;}) }
 #define query_type(TYPE) \
   (object $Message){type_id(TYPE),0}
 #define implements(CLASS,TYPE) \
